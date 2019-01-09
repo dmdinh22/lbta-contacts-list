285f5eb9bfd5244a06bff23942c2c1a964d2d297
David Dinh <dmdinh22@gmail.com>
2019-01-08 21:44:03 -0800

    create table cells

--- lbta-contacts-list/ViewController.swift
+++ lbta-contacts-list/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.title = "Contacts"
         navigationController?.navigationBar.prefersLargeTitles = true
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = "TEST LABEL"
+
+        return cell
+    }
 }
 
